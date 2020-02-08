5684863de6e3db997d0a061f6e64dad2e4a2c3f1
Miriam Del Villar <miriamcdelvillar@Miriams-MacBook-Pro.local>
2020-02-07 20:37:19 -0500

    Miriam Cortez Task04

--- Task 02 a.R
+++ Task 02 a.R
@@ -25,6 +25,9 @@ head(Data)
 beren2 <- Data
 beren3 <- beren2 [order (beren2$age),] 
 write.csv(beren3, "beren_new.csv" , quote=F, row.names=FALSE )
+
+
+Task 02b
 Feeds <- which (beren3$event == "bottle") 
 avgMilk <- mean(beren3$value[Feeds])
 avgFeed <- tapply (beren3$value [Feeds] , beren3$age [Feeds] , mean)
@@ -36,3 +39,10 @@ summary (berenCor)
 berenANOVA <- aov (beren3$value [Feeds] ~ beren3$caregiver [Feeds])
 boxplot (beren3$value [Feeds] ~ beren3$caregiver [Feeds] , xlab= "who gave the bottle" , ylab = "amount of milk consumed (oz)")
 par (las=1, mar=c (5, 5, 1, 1) , mgp=c (2, 0.5, 0) , tck = -0.01)
+plot(as.numeric(names(totalFeed)), totalFeed, type="b", pch=16, xlab="age in days", ylab="ounces of milk")
+abline(h=avgFeed, lty=2, col='red')
+dev.off()
+question #1:
+In hypothesis 1, the weight has a lot of impact on his weight meaning a positive interaction and correlation. In hypothesis 2,  	I think the more he drinks milk, the more he will get sleepy and take a nap. 
+source("http://jonsmitchell.com/code/plotFxn02b.R")
+pdf("r02b-cumulativeMilkByTime.pdf")
